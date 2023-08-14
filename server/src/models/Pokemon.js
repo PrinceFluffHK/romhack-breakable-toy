48e2b4e40b634971e5e2ba383a8790dee68318d0
Garrett Tordo <gtordo3@gmail.com>
2023-08-14 16:05:04 -0400

    removed unnecessary models and migrations, completed model for Pokemon

--- server/src/models/Pokemon.js
+++ server/src/models/Pokemon.js
@@ -1,19 +1,57 @@
-const Model = require("./Model")
+const Model = require("./Model");
 
 class Pokemon extends Model {
     static get tableName() {
-        return "pokemon"
+        return "pokemon";
     }
 
     static get relationMappings() {
-        const { } = require("./index")
+        const { Project } = require("./index");
+        return {
+            project: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: Project,
+                join: {
+                    from: "pokemon.projectId",
+                    to: "project.id"
+                }
+            }
+        }
     }
 
     static get jsonSchema() {
         return {
-            
-        }
+            type: "object",
+            required: [
+                "name",
+                "regionalNum",
+                "nationalNum",
+                "baseHp",
+                "baseAtk",
+                "baseDef",
+                "baseSpA",
+                "baseSpD", 
+                "baseSpe", 
+                "catchRate", 
+                "spriteUrl"
+            ],
+            properties: {
+                name: { type: "string" },
+                regionalNum: { type: ["string", "integer"] },
+                nationalNum: { type: ["string", "integer"] },
+                dexEntry: { type: "string" },
+                baseHp: { type: ["string", "integer"] },
+                baseAtk: { type: ["string", "integer"] },
+                baseDef: { type: ["string", "integer"] },
+                baseSpA: { type: ["string", "integer"] },
+                baseSpD: { type: ["string", "integer"] },
+                baseSpe: { type: ["string", "integer"] },
+                introGen: { type: ["string", "integer"] },
+                catchRate: { type: ["string", "integer"] },
+                spriteUrl: { type: "string" },
+            }
+        };
     }
 }
 
-module.exports = Pokemon
+module.exports = Pokemon;
