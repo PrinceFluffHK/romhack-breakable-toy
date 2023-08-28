f0fda900a248500323165296a4fe6808ef571c0d
Garrett Tordo <gtordo3@gmail.com>
2023-08-28 13:51:00 -0400

    added home page 'about'

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -6,11 +6,29 @@ const HomePage = (props) => {
         <div className="poke-grid">
             <div className="nav-pane-left">
                 <h1 className="right-text">About Romhack Studio:</h1>
-                <p style={{margin: "2rem 3rem"}}>
-                    "Prince Fluff's Romhack Studio" (PFRS) is a combination passion-project and capstone project developed by me, Garrett "Prince Fluff" Tordo. 
-                    Originally started as the capstone project for my coding bootcamp, I knew from the beginning that I wanted whatever I made to be imminently useful for both me and people like me. 
-                    As a 
-                </p>
+                <h4 style={{ margin: "2rem 3rem" }}>
+                    "Prince Fluff's Romhack Studio" (PFRS) is a combination passion-project and
+                    capstone project developed by me, Garrett "Prince Fluff" Tordo. Originally
+                    started as the capstone project for my coding bootcamp, I knew from the
+                    beginning that I wanted whatever I made to be imminently useful for both me and
+                    people like me.
+                </h4>
+                <h4 style={{ margin: "2rem 3rem" }}>
+                    As a now-seasoned Romhacker with two successful hacks released over the last
+                    couple years, I've experienced first-hand the struggles of making good
+                    documentation efficiently. This app intends to relieve much of the stress and
+                    tedium of the documentation process by offering tools custom-made to make every
+                    step of the process more painless.
+                </h4>
+                <h4 style={{ margin: "2rem 3rem" }}>
+                    Features full customization of:
+                    <ul>
+                        <li key={1}>Types</li>
+                        <li key={2}>Abilities</li>
+                        <li key={3}>Stats</li>
+                        <li key={4}>Evolutions</li>
+                    </ul>
+                </h4>
             </div>
             <div className=" ">
                 <h1 className="left-text">Where to?</h1>
